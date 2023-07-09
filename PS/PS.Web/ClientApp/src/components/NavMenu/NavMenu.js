013a27c7c7d4bc035259402e9d0ee29ea8ecf644
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-07-09 12:52:26 +0800

    UI Changes

--- PS/PS.Web/ClientApp/src/components/NavMenu/NavMenu.js
+++ PS/PS.Web/ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
+
+import './NavMenu.css'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
--- PS/PS.Web/ClientApp/src/pages/Customer.js
+++ PS/PS.Web/ClientApp/src/pages/Customer.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Button, FormCheck } from 'react-bootstrap';
+import { PencilSquare, TrashFill } from 'react-bootstrap-icons';
+
+//Utilities
 import { parseDate } from '../utilities/parser';
-import $ from 'jquery';
-import Loader from '../components/Loader';
+
+//Components
+import Loader from '../components/Loader/Loader.js';
 
 const Customer = () => {
     const [customerList, setCustomerList] = useState([]);
@@ -12,7 +17,7 @@ const Customer = () => {
         setTimeout(() => {
             getCustomers();
             setIsLoading(false);
-        }, 1000);
+        }, 500);
     }, [])
 
     function getCustomers(){
@@ -25,61 +30,15 @@ const Customer = () => {
         });
     }
 
-    function loadRow(){
-        return (
-            customerList.map((data) => (
-                <>
-                    <tr key={data.internalID}>
-                        {/* Select Column */}
-                        <td className='cstm-column-select'>
-                            <input class='form-check-input' type='checkbox' value='' id='flexCheckDefault' />
-                        </td>
-                        {/* Customer Column */}
-                        <td>
-                            <div className='cstm-data-detail'>
-                                <span className='name'>{data.name}</span>
-                                <div className='other'>
-                                    <span>
-                                        <i className='bi bi-telephone-fill' />
-                                        {data.contactNo}
-                                    </span>
-                                    <span>
-                                        <i className='bi bi-envelope-fill'></i>
-                                        {data.email}
-                                    </span>
-                                </div>
-                            </div>
-                        </td>
-                        {/* Status Column */}
-                        <td className='cstm-column-status'>
-                            <div className={`cstm-status status-${data.status}`}>
-                                {data.statusDescription}
-                            </div>
-                        </td>
-                        <td className='cstm-column-date'>{parseDate(data.createdDate)}</td>
-                        <td className='cstm-column-date'>{parseDate(data.modifiedDate)}</td>
-                        <td className='cstm-column-action'>
-                            <button type='button' className='btn btn-outline-primary btn-sm me-2' title='Edit Customer'>
-                                <i className='bi bi-pencil-square'></i>
-                            </button>
-                            <button type='button' className='btn btn-outline-danger btn-sm' title='Delete Customer'>
-                                <i className='bi bi-trash-fill'></i>
-                            </button>
-                        </td>
-                    </tr>
-                </>
-            ))
-        )
-    }
-
     return (
         <div>
+            {isLoading && <Loader />}
             <h1>Customers</h1>
             <table className='cstm-table'>
                 <thead>
                     <tr>
                         <th className='cstm-column-select'>
-                            <input class='form-check-input' type='checkbox' value='' id='flexCheckDefault' />
+                            <FormCheck />
                         </th>
                         <th>Customer</th>
                         <th className='cstm-column-status'>Status</th>
@@ -89,14 +48,60 @@ const Customer = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {isLoading ? 
-                        <tr>
-                            <td colSpan={6}>
-                                <Loader />
-                            </td>
-                        </tr>
-                    :
-                        loadRow()
+                    {
+                        customerList.length === 0 ? 
+                            <tr>
+                                <td colSpan="6">No records found in the system.</td>
+                            </tr>
+                        :
+                        customerList.map((data) => (
+                            <>
+                                <tr key={data.internalID}>
+                                    {/* Select Column */}
+                                    <td className='cstm-column-select'>
+                                        <FormCheck />
+                                    </td>
+
+                                    {/* Customer Column */}
+                                    <td>
+                                        <div className='cstm-data-detail'>
+                                            <span className='name'>{data.name}</span>
+                                            <div className='other'>
+                                                <span>
+                                                    <i className='bi bi-telephone-fill' />
+                                                    {data.contactNo}
+                                                </span>
+                                                <span>
+                                                    <i className='bi bi-envelope-fill'></i>
+                                                    {data.email}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                    {/* Status Column */}
+                                    <td className='cstm-column-status'>
+                                        <div className={`cstm-status status-${data.status}`}>
+                                            {data.statusDescription}
+                                        </div>
+                                    </td>
+
+                                    {/* Date Column */}
+                                    <td className='cstm-column-date'>{parseDate(data.createdDate)}</td>
+                                    <td className='cstm-column-date'>{parseDate(data.modifiedDate)}</td>
+
+                                    {/* Action Column */}
+                                    <td className='cstm-column-action'>
+                                        <Button variant='outline-primary' style={{marginRight: '5px'}} size='sm'>
+                                            <PencilSquare />
+                                        </Button>
+                                        <Button variant='outline-danger' size='sm'>
+                                            <TrashFill />
+                                        </Button>
+                                    </td>
+                                </tr>
+                            </>
+                        ))
                     }
                 </tbody>
             </table>
